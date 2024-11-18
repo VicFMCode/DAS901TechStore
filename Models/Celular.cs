cb4fa47280d5d4c8e314d34d5a6d58d458371037
Cduran15 <Carlosduranudb@hotmail.com>
2024-11-17 20:54:05 -0600

    Add files via upload

---
+++ Models/Celular.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechStoreInventory.Models
+{
+    public class Celular
+    {
+        [Key]
+        public int ProductID { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Chip { get; set; }
+        public int Capacidad_in_GB { get; set; }
+        public string Camara { get; set; }
+        public string Color { get; set; }
+        public string Pantalla { get; set; }
+        public decimal Precio { get; set; }
+        public int Existencias { get; set; }
+    }
+}
