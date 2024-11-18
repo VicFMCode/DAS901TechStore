4066c9280cf4d6ac4db1f311b7e201cf282378ad
Cduran15 <Carlosduranudb@hotmail.com>
2024-11-17 20:53:17 -0600

    Add files via upload

---
+++ Data/TechStoreContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechStoreInventory.Models;
+
+namespace TechStoreInventory.Data
+{
+    public class TechStoreContext : DbContext
+    {
+        public TechStoreContext(DbContextOptions<TechStoreContext> options) : base(options) { }
+
+        public DbSet<Celular> Celulares { get; set; }
+    }
+}
